e66699a1fef5387a7aff9697bbc86b6ea1993ddd
BuildTools <unconfigured@null.spigotmc.org>
2018-10-07 18:48:03 +0700

    Some edit for this plugin to make it usable for my server

---
+++ src/main/java/com/darkblade12/particleeffect/ParticleManager.java
@@ -0,0 +1,16 @@
+package com.darkblade12.particleeffect;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ParticleManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Particle manager has been enabled!. By - DrOreo002");
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
